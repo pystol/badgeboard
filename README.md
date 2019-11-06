ce1de9c339e7de13b334fd5cd1629d13836d642b
Carlos Camacho <ccamacho@redhat.com>
2019-11-06 08:33:29 +0100

    First version

--- README.md
+++ README.md
@@ -1,27 +1,83 @@
-![](https://raw.githubusercontent.com/pystol/pystol-docs/master/assets/images/logo_readme.png)
 
-**An open source, self-hosted, chaos engineering toolbox**
+# Badgeboard
 
-  [![Docker image build](https://github.com/pystol/pystol/workflows/docker-image-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=docker-image-build)
-  [![NodeJS install build](https://github.com/pystol/pystol/workflows/nodejs-install-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=nodejs-install-build)
-  [![TOX ESlint build](https://github.com/pystol/pystol/workflows/tox-eslint-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=tox-eslint-build)
-  [![TOX Flake8 build](https://github.com/pystol/pystol/workflows/tox-flake-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=tox-flake-build)
-  [![TOX NodeJS units build](https://github.com/pystol/pystol/workflows/tox-nodeunits-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=tox-nodeunits-build)
-  [![TOX Pytest build](https://github.com/pystol/pystol/workflows/tox-pytest-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=tox-pytest-build)
-  [![TOX SyncMDs build](https://github.com/pystol/pystol/workflows/tox-syncmds-build/badge.svg?event=push)](https://github.com/pystol/pystol/actions?workflow=tox-syncmds-build)
-  [![Docs build](https://github.com/pystol/pystol-docs/workflows/jekyll-docs-build/badge.svg?event=push)](https://github.com/pystol/pystol-docs/actions?workflow=jekyll-docs-build)
-  [![Docker registry STAGING status](https://quay.io/repository/pystol/pystol-operator-staging/status "Docker registry STAGING status")](https://quay.io/repository/pystol/pystol-operator-staging)
-  [![Docker registry STABLE status](https://quay.io/repository/pystol/pystol-operator-stable/status "Docker registry STABLE status")](https://quay.io/repository/pystol/pystol-operator-stable)
-  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-  [![CII Best Practices](https://bestpractices.coreinfrastructure.org/projects/3331/badge)](https://bestpractices.coreinfrastructure.org/projects/3331)
-  [![GitHub issues](https://img.shields.io/github/issues/pystol/pystol)](https://github.com/pystol/pystol/issues)
+This is an awesome information radiator
+to show the status of the badges you
+have in your project.
 
-## Documentation
+![](https://raw.githubusercontent.com/pystol/badgeboard/master/assets/images/badgeboard.png)
 
-Please refer to the [official documentation](https://docs.pystol.org)
-website for any information related to the project.
+A badge is any visual token of achievement,
+affiliation, authorization, or other trust
+relationship.
+The badges are displayed as a CI dashboard.
+
+## Demo
+
+Just [open the index.html](https://www.pystol.org/badgeboard)
+file and see how the dashboard is rendered.
+
+## Requirements
+
+None! Just clone the repo and open the index.html file
+in your favorite browser.
+
+Once you have a copy, make the adjustments to the configuration
+file located in **assets/data_source/badges_list.js** to use your
+own badges.
+
+**Note:** Due to CORS restrictions, this page uses a
+[proxy](https://cors-anywhere.herokuapp.com/)
+to add cross-origin headers when building the widgets panel.
+Check additional information about the CORS proxy on
+[NPM](https://www.npmjs.com/package/cors-anywhere).
+
+## How it works
+
+We capture the badges list (SVG files) and
+we read the color information from a single pixel,
+from there, depending on the color of the pixel the
+widget is painted with its corresponding color.
+
+![](https://raw.githubusercontent.com/pystol/badgeboard/master/assets/images/measure.png)
+
+This would be the ussual view of the project badges.
+
+![](https://raw.githubusercontent.com/pystol/badgeboard/master/assets/images/badges.png)
+
+## Adding your badges and colors
+
+Use the **coordinates_testing.html** file
+to determine based on the SVG coordinates
+the RGB color to be used in the JS configuration
+file.
+
+To do so, copy the link to your badge, find the
+badge example in the file, replace it with yours,
+open the file in a browser, get the console logs
+and move around the mouse over the badge to see
+the coordinates and the RBG color that matches it.
+
+## Adding custom color badges
+
+To add new colors, edit the **assets/css/custom.css** file and
+add new color definitions for the widgets. Once
+you define the new color, in the configuration file
+called **assets/data_source/badges_list.js**
+use the new color like in the following example.
+
+```bash
+colors:[['<new_color_definition','<matching_rgb_from_the _badge>'],['status-good','48,196,82']],
+```
+
+## References
+
+We use both [smashing](https://github.com/smashing/smashing)
+and [gridster](https://github.com/ducksboard/gridster.js)
+to create the dashboard and its widgets.
 
 ## License
 
-Pystol is open source software
-licensed under the [Apache license](LICENSE).
+Badgeboard is part of Pystol and Pystol is
+open source software licensed under the
+[Apache license](LICENSE).
---
+++ assets/data_source/badges_list.js
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2019 pystol.org - All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: Carlos Camacho <carloscamachoucv@gmail.com>
+ */
+
+var corssproxy = 'https://cors-anywhere.herokuapp.com/';
+var badges = [
+{
+  type: 'text',
+	size:{x:2,y:1},
+	position:{x:1,y:1},
+	title: "The chaos toolbox",
+	subtitle: 'CI dashboard status',
+	description: 'GitHub actions reports',
+	link: 'http://www.bing.com',
+	color:'widget-text',
+},
+{
+  type: 'picture',
+	size:{x:2,y:1},
+	position:{x:3,y:1},
+	src:'assets/images/logo.png',
+	color:'widget-logo',
+},
+{
+  type: 'clock',
+	size:{x:2,y:1},
+	position:{x:5,y:1},
+	color:'widget-meter',
+},
+{
+  type: 'badge',
+	size:{x:1,y:1},
+	position:{x:1,y:2},
+  uri: corssproxy + 'https://github.com/pystol/pystol/workflows/docker-image-build/badge.svg?event=push',
+	title: "Docker build",
+	description: 'Local image creation',
+	link: 'http://www.bing.com',
+	measure:{x:175,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+	size:{x:1,y:1},
+	position:{x:2,y:2},
+  uri: corssproxy + 'https://github.com/pystol/pystol/workflows/nodejs-install-build/badge.svg?event=push',
+	title: "NodeJS",
+	description: 'Installing NodeJS',
+	link: 'http://www.bing.com',
+	measure:{x:175,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:3,y:2},
+	uri: corssproxy + 'https://github.com/pystol/pystol/workflows/tox-nodeunits-build/badge.svg?event=push',
+	title: "NodeJS",
+	description: 'Tox unit testing',
+	link: 'http://www.bing.com',
+	measure:{x:163,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:4,y:2},
+	uri: corssproxy + 'https://github.com/pystol/pystol/workflows/tox-flake-build/badge.svg?event=push',
+	title: "Flake8",
+	description: 'Tox Python syntax',
+	link: 'http://www.bing.com',
+	measure:{x:127,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:5,y:2},
+	uri: corssproxy + 'https://github.com/pystol/pystol/workflows/tox-pytest-build/badge.svg?event=push',
+	title: "Pytest",
+	description: 'Tox Python unit testing',
+	link: 'http://www.bing.com',
+	measure:{x:143,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:6,y:2},
+	uri: corssproxy + 'https://github.com/pystol/pystol/workflows/tox-eslint-build/badge.svg?event=push',
+	title: "ESlint",
+	description: 'Tox JS linting job',
+	link: 'http://www.bing.com',
+	measure:{x:140,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:1,y:3},
+	uri: corssproxy + 'https://github.com/pystol/pystol/workflows/tox-syncmds-build/badge.svg?event=push',
+	title: "Sync MDs'",
+	description: 'Common files sync check',
+	link: 'http://www.bing.com',
+	measure:{x:143,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:2,y:3},
+	uri: corssproxy + 'https://github.com/pystol/pystol-docs/workflows/jekyll-docs-build/badge.svg?event=push',
+	title: "Pystol docs",
+	description: 'Documentation build',
+	link: 'http://www.bing.com',
+	measure:{x:133,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:3,y:3},
+	uri: corssproxy + 'https://quay.io/repository/pystol/pystol-operator-staging/status',
+	title: "Quay.io",
+	description: 'Staging image status',
+	link: 'http://www.bing.com',
+	measure:{x:81,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','149,195,14']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:4,y:3},
+	uri: corssproxy + 'https://quay.io/repository/pystol/pystol-operator-stable/status',
+	title: "Quay.io",
+	description: 'Stable image status',
+	link: 'http://www.bing.com',
+  measure:{x:81,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','149,195,14']],
+},
+{
+	type: 'badge',
+	size:{x:1,y:1},
+	position:{x:1,y:4},
+  uri: corssproxy + 'https://img.shields.io/github/issues/pystol/pystol',
+	title: "GitHub issues",
+	description: 'Operator bug reports',
+	link: 'https://github.com/pystol/pystol/issues',
+	measure:{x:61,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','75,197,29']],
+},
+{
+	type: 'badge',
+	size:{x:1,y:1},
+	position:{x:2,y:4},
+  uri: corssproxy + 'https://bestpractices.coreinfrastructure.org/projects/3331/badge',
+	title: "CII",
+	description: 'Best practices check',
+	link: 'https://bestpractices.coreinfrastructure.org/en/projects/3331',
+	measure:{x:170,y:3},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82']],
+},
+{
+  type: 'badge',
+ 	size:{x:1,y:1},
+ 	position:{x:3,y:4},
+	uri: corssproxy + 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+	title: "License",
+	description: 'Apache license check',
+	link: 'http://www.bing.com',
+	measure:{x:93,y:5},
+	colors:[['status-warning','212,52,67'],['status-danger','197,105,30'],['status-good','48,196,82'],['widget-ocean','14,127,191']],
+},
+];
